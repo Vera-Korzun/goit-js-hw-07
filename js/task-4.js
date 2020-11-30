0834de130f784ca99c5315b93281e3a3677eb0fb
Vera Korzun <veraminich@gmail.com>
2020-11-30 15:07:49 +0100

    Добавляет 4 задачу

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+let counterValue=0;
+const getDiv=document.querySelector('#counter')
+let getSpam=document.querySelector('#value')
+let clickDecrement=document.querySelector('button[data-action="decrement"]')
+let clickIncrement=document.querySelector('button[data-action="increment"]')
+
+const decrement=function(){
+    counterValue-=1;
+    value.textContent=counterValue;
+}
+
+const increment=function(){
+    counterValue+=1;
+    value.textContent=counterValue;
+}
+
+clickDecrement.addEventListener('click',decrement);
+clickIncrement.addEventListener('click',increment);
